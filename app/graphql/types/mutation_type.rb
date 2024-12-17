9a4567c228006c559a33c364d88a9f9583728426
Alejo-Rey <alejo.rey.128@gmail.com>
2024-12-16 20:04:12 -0500

    -change: structure for all the queries

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,12 +3,5 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_event, mutation: Mutations::CreateEvent
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,22 +2,10 @@
 
 module Types
   class QueryType < Types::BaseObject
+    include Queries::EventQueries
+
     field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
       argument :id, ID, required: true, description: "ID of the object."
-    end
-
-    field :events, [EventType], null: false, description: "Fetch all events"
-    field :event, EventType, null: true do
-      description "Obtener un evento específico por ID"
-      argument :id, ID, required: true
-    end
-
-    def events
-      Event.all
-    end
-
-    def event(id:)
-      Event.find_by(id: id)
     end
 
     def node(id:)
@@ -30,16 +18,6 @@ module Types
 
     def nodes(ids:)
       ids.map { |id| context.schema.object_from_id(id, context) }
-    end
-
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
     end
   end
 end
